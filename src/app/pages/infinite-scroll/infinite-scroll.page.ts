068a39faa4afb4c839dde7c9799ce36325f31508
issa23i <ipaslop262@iesmartinezm.es>
2022-11-16 13:01:50 +0100

    infinite scroll

--- src/app/pages/infinite-scroll/infinite-scroll.page.ts
+++ src/app/pages/infinite-scroll/infinite-scroll.page.ts
@@ -1,4 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+
+// @Component({
+//   selector: 'app-infinite-scroll',
+//   templateUrl: './infinite-scroll.page.html',
+//   styleUrls: ['./infinite-scroll.page.scss'],
+// })
+// export class InfiniteScrollPage implements OnInit {
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-infinite-scroll',
@@ -7,9 +23,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfiniteScrollPage implements OnInit {
 
+  data: any[] = Array(20);
+
+  @ViewChild( IonInfiniteScroll ) inifiteScroll: IonInfiniteScroll;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  loadData( event ) {
+
+    // console.log(event);
+
+    setTimeout(() => {
+
+      if ( this.data.length > 50 ) {
+        this.inifiteScroll.complete();
+        this.inifiteScroll.disabled = true;
+        return;
+      }
+
+      const nuevoArr = Array(20);
+      this.data.push( ...nuevoArr );
+
+      // event.target.complete();
+      this.inifiteScroll.complete();
+    }, 1500);
+
   }
 
 }
